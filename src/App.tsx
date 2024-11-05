5a630de1e0a7dc34270db0559d9e349da1538770
Artemis-Hunt <stevetheguy@naver.com>
2024-11-05 17:37:58 -0500

    Updated default font to Kaisei Tokumin

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,42 @@
 import { useState } from "react";
 import "./App.css";
 import { Button, BottomNavigation, Paper } from "@mui/material";
-import { typography } from '@mui/system';
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import HomeIcon from '@mui/icons-material/Home';
-import AppsIcon from '@mui/icons-material/Apps';
-import SearchIcon from '@mui/icons-material/Search';
-import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
-import SettingsIcon from '@mui/icons-material/Settings';
+import HomeIcon from "@mui/icons-material/Home";
+import AppsIcon from "@mui/icons-material/Apps";
+import SearchIcon from "@mui/icons-material/Search";
+import DynamicFeedIcon from "@mui/icons-material/DynamicFeed";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [value, setValue] = useState(0);
 
   return (
     <>
-    <Button>
-      Test123
-    </Button>
-    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-    <BottomNavigation
-      showLabels
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      
-    >
-      <BottomNavigationAction label="Home" sx={{fontFamily: 'serif', fontStyle: 'oblique'}} icon={<HomeIcon />} />
-      <BottomNavigationAction label="Categories" icon={<AppsIcon />} />
-      <BottomNavigationAction label="Explore" icon={<SearchIcon />} />
-      <BottomNavigationAction label="Brands" icon={<DynamicFeedIcon />} />
-      <BottomNavigationAction label="Settings" icon={<SettingsIcon />} />
-    </BottomNavigation>
-  </Paper>
-  </>
+      <Button>Test123</Button>
+      <Paper
+        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction
+            label="Home"
+            sx={{ fontFamily: '"Kaisei Tokumin"' }}
+            icon={<HomeIcon />}
+          />
+          <BottomNavigationAction label="Categories" icon={<AppsIcon />} />
+          <BottomNavigationAction label="Explore" icon={<SearchIcon />} />
+          <BottomNavigationAction label="Brands" icon={<DynamicFeedIcon />} />
+          <BottomNavigationAction label="Settings" icon={<SettingsIcon />} />
+        </BottomNavigation>
+      </Paper>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      '"Kaisei Tokumin"',
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
